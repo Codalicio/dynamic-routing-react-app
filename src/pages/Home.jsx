3195a4ec7b03f8a82f79bdd67634709591e09c54
Amit Raj <rajamitnine@gmail.com>
2025-05-02 13:54:49 +0530

    Add a method to handle the user state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,10 @@ const Home = ({ user, setUser }) => {
     navigate(`/${user}`);
     setUser("");
   };
+
+  const emptyUser = () => {
+    setUser("");
+  };
   return (
     <div className="w-[90%] bg-red-700 mx-auto flex justify-center mt-10 p-8 rounded max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center">
       <input
@@ -32,6 +36,7 @@ const Home = ({ user, setUser }) => {
       </a>
       <Link
         to={`/${user}`}
+        onClick={emptyUser}
         className="bg-green-600 text-white p-2 ml-5 mx-7 rounded border-none cursor-pointer text-center max-sm:w-[80%] max-sm:ml-7"
       >
         via Link tag
